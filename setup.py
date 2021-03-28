c31a3027b39c8d087b431d1ad44f1c7bc7f4523d
Cesar Arcos <60860385+racec9999@users.noreply.github.com>
2021-03-28 13:46:34 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,21 @@
 import setuptools
 
-with open('README.md','r') as file:
-    long_desc = file.read()
+with open('README.md', 'r') as file:
+	long_description = file.read()
 
 
 setuptools.setup(
-    name = 'preprocess_nlp_Cesar_Arcos', # this should be unique
-    version = '0.0.1',
-    author = 'Cesar Arcos Gonzalez',
-    author_email = 'cesar99ag@gmail.com',
-    description = 'This is preprocessing package'
-    long_description = long_desc,
-    long_description_content_type = 'text/markdown'
-    packages = setuptools.find_packages(),
-    classifiers = [
-        'Programming Language :: Python :: 3',
-        'License :: OSI Aproved :: MIT License',
-        "Operating System :: OS Independent"],
-    python_requires = '>=3.5'
-)
-
-
-
-
+	name = 'preprocess_nlp_Cesar_Arcos', #this should be unique
+	version = '0.0.1',
+	author = 'Cesar Arcos Gonzalez',
+	author_email = 'cesar99ag@gmail.com',
+	description = 'This is preprocessing package',
+	long_description = long_description,
+	long_description_content_type = 'text/markdown',
+	packages = setuptools.find_packages(),
+	classifiers = [
+	'Programming Language :: Python :: 3',
+	'License :: OSI Aproved :: MIT License',
+	"Operating System :: OS Independent"],
+	python_requires = '>=3.5'
+	)
